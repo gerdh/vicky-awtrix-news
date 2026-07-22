4fdd3fd264247287ff9dda453661be817eefc0be
gerdh <gerd.holzhauer@gmail.com>
2026-07-22 11:57:28 +0200

    Add colors.py for V6

---
+++ colors.py
@@ -0,0 +1,17 @@
+COLORS = {
+    "france": "66CCFF",
+    "spiegel": "FFCC00",
+    "bbc": "66FF66",
+    "guardian": "00CC66",
+
+    "finance": "00FFFF",
+    "tech": "AA66FF",
+    "weather": "00AAFF",
+
+    "battery_ok": "00FF00",
+    "warning": "FF8800",
+    "critical": "FF0000",
+
+    # niemals weiß benutzen
+    "neutral": "66CCFF",
+}
