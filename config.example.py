a9f6cbce4d1750d71fc0bc4c5167ec6a702c4897
gerdh <gerd.holzhauer@gmail.com>
2026-08-28 15:17:17 +0200

    V8: remove obsolete llama settings

--- config.example.py
+++ config.example.py
@@ -6,8 +6,5 @@ MQTT_PASS = "your_mqtt_password"
 
 BASE_TOPIC = "awtrix_xxxxxx/custom"
 
-LLAMA_API_URL = "http://127.0.0.1:8080/v1/chat/completions"
-LLAMA_MODEL = "vicky"
-
 MAX_DISPLAY_TEXT = 180
 DEFAULT_DURATION = 20
